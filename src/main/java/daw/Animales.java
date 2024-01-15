66fe236c37fa73dcaa95903219122d54ca393ebe
nuria <nrioolm101@g.educaand.es>
2024-01-15 09:39:48 +0100

    Atributos

---
+++ src/main/java/daw/Animales.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package daw;
+
+/**
+ *
+ * @author nuria
+ */
+public class Animales {
+
+        private String especie;
+        private double peso;
+        private String nombre;
+        
+        public Animales(String especie, String nombre, double peso){
+            this.especie=especie;
+            this.nombre=nombre;
+            this.peso=peso;
+        }
+    
+}
